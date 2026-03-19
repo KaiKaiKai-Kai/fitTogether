53fb043c055d63f773674cacc0344b9120c08bdd
KaiMeyburgh <dekamakazi@gmail.com>
2026-03-19 17:22:07 +0100

    hosting

---
+++ public/firebase.js
@@ -0,0 +1,19 @@
+<script type="module">
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.10.0/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyA7b1_-yb9haf5Yy9tTgdbcsAyKxKVv-ew",
+    authDomain: "fittogether-kai.firebaseapp.com",
+    projectId: "fittogether-kai",
+    storageBucket: "fittogether-kai.firebasestorage.app",
+    messagingSenderId: "486479209386",
+    appId: "1:486479209386:web:351fb651ab5b4c94d981d3"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+</script>
