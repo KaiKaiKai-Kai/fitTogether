fe0f9cc8bba699be4e501eb4cf94eca94a0f1f25
kai <dekamakazi@gmail.com>
2026-03-19 18:21:12 +0100

    Update firebase.js

--- public/firebase.js
+++ public/firebase.js
@@ -5,12 +5,12 @@ import { getFirestore }    from "https://www.gstatic.com/firebasejs/10.12.0/fire
 import { getAuth }         from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
  
   const firebaseConfig = {
-    apiKey: "AIzaSyA7b1_-yb9haf5Yy9tTgdbcsAyKxKVv-ew",
-    authDomain: "fittogether-kai.firebaseapp.com",
-    projectId: "fittogether-kai",
-    storageBucket: "fittogether-kai.firebasestorage.app",
-    messagingSenderId: "486479209386",
-    appId: "1:486479209386:web:351fb651ab5b4c94d981d3"
+    apiKey: "no",
+    authDomain: "no",
+    projectId: "no",
+    storageBucket: "no",
+    messagingSenderId: "no",
+    appId: "no"
 };
  
 const app = initializeApp(firebaseConfig);
